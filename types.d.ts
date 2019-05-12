a27c0bf163a27a3abf8c7d62a13b5f5d45a6b2ed
psaux <seriouz.accz@gmail.com>
2019-05-12 18:20:43 +0200

    Try to fix types [2]

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,13 @@
-import HttpClient from 'src/http/HttpClient'
 
-export type ApiHttpClient = HttpClient
+export type HttpResponse = Promise<any> & {
+    expectStatus: (status: number) => HttpResponse
+    expectType: (type: any) => HttpResponse
+    data: () => Promise<any>
+}
+
+export type ApiHttpClient = {
+    get: (url: string, opts?: any) => HttpResponse
+    post: (url: string, data: any) => HttpResponse
+    put: (url: string, data: any) => HttpResponse
+    delete: (url: string) => HttpResponse
+}
